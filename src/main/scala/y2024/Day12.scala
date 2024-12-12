c5a51c1c59b1a001f9c3bb3fd3626f75ad11afec
Wade Sherman <wade.sherman@disneystreaming.com>
2024-12-12 13:38:49 -0800

    2024 12 tidy

--- src/main/scala/y2024/Day12.scala
+++ src/main/scala/y2024/Day12.scala
@@ -3,8 +3,9 @@ package y2024
 import model._
 import util.ProvidedInput
 
-object Day12 extends ProvidedInput with App {
+import scala.annotation.tailrec
 
+object Day12 extends ProvidedInput with App {
 
   override val year: Int = 2024
   override val day: Int = 12
@@ -18,7 +19,7 @@ object Day12 extends ProvidedInput with App {
     "VVIIICJJEE",
     "MIIIIIJJEE",
     "MIIISIJEEE",
-    "MMMISSJEEE",
+    "MMMISSJEEE"
   )
 
   type Grid = Map[Position, Node[Char]]
@@ -34,12 +35,13 @@ object Day12 extends ProvidedInput with App {
   type Plots = List[Plot]
   type Region = List[Plots]
 
-  def plots(grid: Grid): List[List[Plot]] = {
+  def plots(grid: Grid): Region = {
     val groups: List[List[Plot]] = Nil
     val visited: Set[Position] = Set.empty[Position]
 
-    def collectGroup(nodes: List[Node[Char]], visited: Set[Plot]): List[Plot] = {
-      if(nodes.isEmpty){
+    @tailrec
+    def collectGroup(nodes: List[Node[Char]], visited: Set[Plot]): Plots = {
+      if (nodes.isEmpty) {
         visited.toList
       } else {
         val acc: List[(Plot, List[Node[Char]])] = nodes
@@ -50,36 +52,31 @@ object Day12 extends ProvidedInput with App {
             val fenceNeighbors = allNeighbors.filter(an => !nodeNeighbors.map(_.position).contains(an.position))
             val fenceCount = fenceNeighbors.length
             (Plot(n, fenceCount, fenceNeighbors), nodeNeighbors)
-        })
+          })
         collectGroup(acc.flatMap(_._2).distinct, visited ++ acc.map(_._1))
       }
     }
 
-    val plots = grid.foldLeft((groups, visited)){ case ((g, v), (_, n)) => {
-      if(v.contains(n.position)){
-        (g, v)
-      } else {
-        val newGroup: List[Plot] = collectGroup(List(n), Set.empty[Plot])
-        (g :+ newGroup, v ++ newGroup.map(_.node.position))
+    val plots = grid.foldLeft((groups, visited)) {
+      case ((g, v), (_, n)) => {
+        if (v.contains(n.position)) {
+          (g, v)
+        } else {
+          val newGroup: List[Plot] = collectGroup(List(n), Set.empty[Plot])
+          (g :+ newGroup, v ++ newGroup.map(_.node.position))
+        }
       }
-    }}
+    }
     plots._1
   }
 
-  def cost(region: Region) = {
-    region.map(p => {
-      val area = p.length
-      val fences = p.map(_.fences).sum
-      area * fences
-    }).sum
-  }
-
-
-  def countFences(v: List[model.Vector], d: Direction): Int = {
-    val comp: (Vector, Vector) => Boolean ={ case (last, n) =>  d match {
-      case N | S => last.position.x == n.position.x - 1 && last.position.y == n.position.y
-      case E | W => last.position.x == n.position.x && last.position.y == n.position.y - 1
-    }}
+  def countSides(v: List[model.Vector], d: Direction): Int = {
+    val comp: (Vector, Vector) => Boolean = { case (last, n) =>
+      d match {
+        case N | S => last.position.x == n.position.x - 1 && last.position.y == n.position.y
+        case E | W => last.position.x == n.position.x && last.position.y == n.position.y - 1
+      }
+    }
     val sorted = d match {
       case N | S => v.sortBy(n => (n.position.y, n.position.x))
       case E | W => v.sortBy(n => (n.position.x, n.position.y))
@@ -87,27 +84,44 @@ object Day12 extends ProvidedInput with App {
     sorted match {
       case Nil => 0
       case head :: next => {
-        next.foldLeft((head, 1)){ case ((last, c), n) => {
-          if(comp(last, n)){
-            (n, c)
-          } else {
-            (n, c + 1)
+        next
+          .foldLeft((head, 1)) {
+            case ((last, c), n) => {
+              if (comp(last, n)) {
+                (n, c)
+              } else {
+                (n, c + 1)
+              }
+            }
           }
-        }}._2
+          ._2
       }
     }
   }
 
-  def bulkCost(region: Region) = {
-    region.map(p => {
-      val area = p.length
-      val fences = p
-        .filter(_.fences > 0)
-        .flatMap(_.fenceNeighbors)
-        .groupBy(_.direction)
-        .map{ case (d, v) => countFences(v, d)}.sum
+  def cost(region: Region): Int = {
+    region
+      .map(p => {
+        val area = p.length
+        val fences = p.map(_.fences).sum
         area * fences
-    }).sum
+      })
+      .sum
+  }
+
+  def bulkCost(region: Region): Int = {
+    region
+      .map(p => {
+        val area = p.length
+        val fences = p
+          .filter(_.fences > 0)
+          .flatMap(_.fenceNeighbors)
+          .groupBy(_.direction)
+          .map { case (d, v) => countSides(v, d) }
+          .sum
+        area * fences
+      })
+      .sum
   }
 
   assert(cost(plots(parseInput(testInput))) == 1930)
